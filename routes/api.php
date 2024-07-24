7a44714c0f7dc8676cafffe589fee7c5d8de3f7e
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-07-25 00:05:27 +0100

    paystack integration

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaymentController;
 
 Route::post('/initialize-payment', [PaymentController::class, 'initializePayment']);
-Route::get('/verify-transaction', [PaymentController::class, 'verifyTransaction']);
+Route::post('/verify-transaction', [PaymentController::class, 'verifyTransaction']);
 Route::get('/transactions', [PaymentController::class, 'listTransactions']);
